2343534ee6f6bbd1a75901d7e2145a75a8ce0f76
eireann07 <101282477+eireann07@users.noreply.github.com>
2022-04-20 17:44:46 -0500

    Initial commit

---
+++ weather_bot.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 16:41:24 2022
+
+@author: Erin
+"""
+
+from tkinter import *
+from tkinter import ttk
+import requests
+import json
+from datetime import datetime
+
+#Initialize Window
+ 
+root =Tk()
+root.geometry("400x400") #size of the window by default
+root.resizable(0,0) #to make the window size fixed
+#title of our window
+root.title('Weather App')
+
+#Display Weather
+city_value = StringVar()
+
+def time_format_for_location(utc_with_tz):
+    local_time = datetime.utcfromtimestamp(utc_with_tz)
+    return local_time.time()
+
+city_value = StringVar()
+
+def showWeather():
+    api_key = '2b65828fe78024e0991a384d02b55d77'
+    city_name=city_value.get()
+    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=' +api_key
+    response = requests.get(weather_url)
+    weather_info = response.json()
+    
+    tfield.delete('1.0', 'end')
+    
+    if weather_info['cod'] == 200:
+        kelvin = 273
+        
+        temp = int(weather_info['main']['temp'] - kelvin)
+        feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
+        pressure = weather_info['main']['pressure']
+        humidity = weather_info['main']['humidity']
+        wind_speed = weather_info['wind']['speed'] * 3.6
+        sunrise = weather_info['sys']['sunrise']
+        sunset = weather_info['sys']['sunset']
+        timezone = weather_info['timezone']
+        cloudy = weather_info['clouds']['all']
+        description = weather_info['weather'][0]['description']
+        
+        sunrise_time = time_format_for_location(sunrise + timezone)
+        sunset_time = time_format_for_location(sunset + timezone)
+        
+        weather = f'\nWeather of: {city_name}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}&\nInfo: {description}'
+    else:
+        weather = f'\n\tWeather for {city_name} not found!\n\tKindly enter valid city name!'
+        
+    tfield.insert(INSERT, weather)
+    
+# Front End Code
+city_head = Label(root, text = 'Enter city name', font = 'Arial 12 bold').pack(pady=10)
+
+inp_city = Entry(root, textvariable = city_value, width = 24, font = 'Arial 14 bold').pack()
+    
+Button(root, command = showWeather, text = 'Check weather', font = 'Arial 10', bg = 'lightblue', fg = 'black', activebackground = 'teal', padx=5, pady=5 ).pack(pady=20)
+    
+weather_now = Label(root, text = 'The weather is:', font = 'Arial 12 bold').pack(pady=10)
+    
+tfield = Text(root, width=46, height=10)
+tfield.pack()
+    
+root.mainloop()
+
+        
