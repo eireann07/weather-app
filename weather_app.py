f4f1a81e04d8067f6850e98e37a064a51c36cf73
eireann07 <101282477+eireann07@users.noreply.github.com>
2022-04-20 17:46:08 -0500

    Proofread 1

--- weather_app.py
+++ weather_app.py
@@ -15,7 +15,7 @@ from datetime import datetime
 #Initialize Window
  
 root =Tk()
-root.geometry("400x400") #size of the window by default
+root.geometry("400x400") #size of the window - default
 root.resizable(0,0) #to make the window size fixed
 #title of our window
 root.title('Weather App')
